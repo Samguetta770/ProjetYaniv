554ddb5f48fffdc0611308b7713bf90bfceaa79d
samos <samuelguetta@campus.technion.ac.il>
2024-12-11 15:43:58 +0200

    Fix erreurs production et configuration Vercel

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Route non trouvée.' });
+});
+
+
 // Gestion des erreurs
 app.use(function (err, req, res, next) {
   // Définir les détails de l'erreur
