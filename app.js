3feee5f072f51af2c48caa87424aee5ef969cc05
samos <samuelguetta@campus.technion.ac.il>
2024-12-11 17:48:26 +0200

    Fix erreurs production et configuration Verceléééééééé

--- app.js
+++ app.js
@@ -1,51 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// Configuration de Pug pour les vues (optionnel si non utilisé)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app = express();
 
 // Middlewares
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// **Servir les fichiers statiques avant les routes**
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Routes principales
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-app.use((req, res, next) => {
-    res.status(404).json({ error: 'Route non trouvée.' });
-});
-
-
-// Gestion des erreurs
-app.use(function (err, req, res, next) {
-  // Définir les détails de l'erreur
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Rendu de la page d'erreur
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// Exporter l'application
 module.exports = app;
